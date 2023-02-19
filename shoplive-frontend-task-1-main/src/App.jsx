ad1d47f17d0d2e01b759ae26c425d8328df7a333
pangkyu <baesense@naver.com>
2023-02-19 20:40:06 +0900

    Style : 모바일 뷰 적용
    모바일 뷰(480px)기준 뷰 적용
    - grid 적용

--- shoplive-frontend-task-1-main/src/App.jsx
+++ shoplive-frontend-task-1-main/src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.scss";
 import Header from "./components/Header";
+import date from "./utils/format/date";
+import Logo from "./components/Logo";
 import { DUMMY } from "./dummies";
 import DescriptionComponent from "./DescriptionComponent";
 import { getRandomText } from "./random-text";
@@ -23,11 +25,30 @@ const App = () => {
 
       <div>아이템 - 총 5 개</div>
 
-      <div className="wrap-items">
+      <div className="contents">
         {DUMMY.map((item) => (
-          <div key={item.id} className="item-row">
-            {/* <img src={item.imageUrl} /> */}
-            <div>{item.title}</div>
+          <div key={item.id} className="contents__item">
+            <div className="contents__item-logo">
+              <Logo />
+            </div>
+            <img
+              src={item.imageUrl}
+              className="contents__item-image"
+              alt="더미이미지"
+            />
+            <div className="contents__item-likeCount">
+              LIKE : {item.likeCount}
+            </div>
+            <div className="contents__item-TextBox">
+              <div className="contents__item-createdAt">
+                {date(item.createdAt)}
+              </div>
+              <div className="contents__item-title">{item.title}</div>
+            </div>
+            <div className="contents__item-button">
+              <button className="contents__item-update">수정</button>
+              <button className="contents__item-remove">제거</button>
+            </div>
           </div>
         ))}
       </div>
--- shoplive-frontend-task-1-main/src/components/Logo.jsx
+++ shoplive-frontend-task-1-main/src/components/Logo.jsx
@@ -1,4 +1,5 @@
 import "../scss/logo.scss";
+
 const Logo = () => {
   return <span className="logo">ABCDEFGH</span>;
 };
