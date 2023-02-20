6a3d9199aab2494c2d42c2b0087fbb5748d7d401
pangkyu <baesense@naver.com>
2023-02-21 01:48:25 +0900

    Feat : 로컬스토리지 데이터 저장 기능
    setItem을 사용하여 로컬스토리지에 데이터를 저장할 수 있도록 했습니다.
    - Key값도 id, createdAt와 같이 TIMESTAMP값으로 입력받아 유니크하게 관리할 수 있도록 구현했습니다.
    -

--- shoplive-frontend-task-1-main/src/App.jsx
+++ shoplive-frontend-task-1-main/src/App.jsx
@@ -5,22 +5,63 @@ import Logo from "./components/Logo";
 import Search from "./components/Search";
 import { DUMMY } from "./dummies";
 import DescriptionComponent from "./DescriptionComponent";
-import { getRandomText } from "./random-text";
 
 const App = () => {
+  const handleAddBtn = async (e) => {
+    e.preventDefault();
+    alert("클릭");
+    const inputTitle = document.querySelector(".title").value;
+    const inputLikeCount = document.querySelector(".likeCount").value;
+    const inputImageUrl = document.querySelector(".imageUrl").value;
+    const inputId = Date.now();
+    const inputCreatedAt = Date.now();
+    console.log(inputTitle);
+    console.log(inputLikeCount);
+    console.log(inputImageUrl);
+    console.log(inputId);
+    localStorage.setItem(
+      inputId,
+      JSON.stringify({
+        id: inputId,
+        title: inputTitle,
+        createdAt: inputCreatedAt,
+        likeCount: inputLikeCount,
+        imageUrl: inputImageUrl,
+      })
+    );
+
+    // localStorage.setItem("title", title);
+
+    // const dataArray = JSON.parse(localStorage.getItem("dataList")) || [];
+    // // dataArray.push(data);
+    // localStorage.setItem("datalist", JSON.stringify(dataArray));
+  };
+
   return (
     <div className="App">
       {/* <DescriptionComponent /> */}
       <Header />
-      <div>
-        <input placeholder="title" />
-        <input placeholder="likeCount" />
-        <input placeholder="imageUrl" />
-        <button>추가</button>
-      </div>
+      <form className="contents-form">
+        <div className="contents__addBar">
+          <input placeholder="title" type="text" className="title" required />
+          <input
+            placeholder="likeCount"
+            type="number"
+            className="likeCount"
+            required
+          />
+          <input
+            placeholder="imageUrl"
+            type="text"
+            className="imageUrl"
+            required
+          />
+          <button onClick={handleAddBtn}>추가</button>
+        </div>
+      </form>
       <div className="contents__bar">
         <div className="contents__bar-itemAmount">
-          아이템 - 총 {DUMMY.length} 개
+          아이템 - 총 {DUMMY.length + (window.localStorage.length - 2)} 개
         </div>
         <Search className="contents__bar-search" />
       </div>
