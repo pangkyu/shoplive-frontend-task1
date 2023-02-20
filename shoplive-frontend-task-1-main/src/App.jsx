f2b9bba368ec20dc29d5787c8b6084dcf56b69b4
pangkyu <baesense@naver.com>
2023-02-21 02:41:30 +0900

    Style : 로컬 스토리지 데이터 UI 출력
    - 아이템 개수 : 더미데이터 + 로컬스토리지 합
    - 로컬스토리지 키 값 돌면서 값 ui에 출력 하도록 설정

--- shoplive-frontend-task-1-main/src/App.jsx
+++ shoplive-frontend-task-1-main/src/App.jsx
@@ -5,36 +5,38 @@ import Logo from "./components/Logo";
 import Search from "./components/Search";
 import { DUMMY } from "./dummies";
 import DescriptionComponent from "./DescriptionComponent";
+import { useEffect } from "react";
 
 const App = () => {
-  const handleAddBtn = async (e) => {
-    e.preventDefault();
-    alert("클릭");
+  let keys = Object.keys(localStorage);
+  let itemArray = [];
+  for (let key of keys) {
+    // console.log(`${key} : ${localStorage.getItem(key)}`);
+    // console.log(JSON.parse(localStorage.getItem(key)));
+    itemArray.push(JSON.parse(localStorage.getItem(key)));
+  }
+  console.log(itemArray);
+
+  const handleAddBtn = async () => {
     const inputTitle = document.querySelector(".title").value;
     const inputLikeCount = document.querySelector(".likeCount").value;
     const inputImageUrl = document.querySelector(".imageUrl").value;
     const inputId = Date.now();
     const inputCreatedAt = Date.now();
-    console.log(inputTitle);
-    console.log(inputLikeCount);
-    console.log(inputImageUrl);
-    console.log(inputId);
-    localStorage.setItem(
-      inputId,
-      JSON.stringify({
-        id: inputId,
-        title: inputTitle,
-        createdAt: inputCreatedAt,
-        likeCount: inputLikeCount,
-        imageUrl: inputImageUrl,
-      })
-    );
-
-    // localStorage.setItem("title", title);
-
-    // const dataArray = JSON.parse(localStorage.getItem("dataList")) || [];
-    // // dataArray.push(data);
-    // localStorage.setItem("datalist", JSON.stringify(dataArray));
+    if (inputTitle && inputLikeCount && inputImageUrl !== "") {
+      localStorage.setItem(
+        inputId,
+        JSON.stringify({
+          id: inputId,
+          title: inputTitle,
+          createdAt: inputCreatedAt,
+          likeCount: Number(inputLikeCount),
+          imageUrl: inputImageUrl,
+        })
+      );
+    } else {
+      alert("비어있는 항목이 있습니다. ");
+    }
   };
 
   return (
@@ -61,12 +63,37 @@ const App = () => {
       </form>
       <div className="contents__bar">
         <div className="contents__bar-itemAmount">
-          아이템 - 총 {DUMMY.length + (window.localStorage.length - 2)} 개
+          아이템 - 총 {DUMMY.length + window.localStorage.length} 개
         </div>
         <Search className="contents__bar-search" />
       </div>
       <div className="contents">
         {DUMMY.map((item) => (
+          <div key={item.id} className="contents__item">
+            <div className="contents__item-logo">
+              <Logo />
+            </div>
+            <img
+              src={item.imageUrl}
+              className="contents__item-image"
+              alt="더미이미지"
+            />
+            <div className="contents__item-likeCount">
+              LIKE : {item.likeCount}
+            </div>
+            <div className="contents__item-TextBox">
+              <div className="contents__item-createdAt">
+                {date(item.createdAt)}
+              </div>
+              <div className="contents__item-title">{item.title}</div>
+            </div>
+            <div className="contents__item-button">
+              <button className="contents__item-update">수정</button>
+              <button className="contents__item-remove">제거</button>
+            </div>
+          </div>
+        ))}
+        {itemArray.map((item) => (
           <div key={item.id} className="contents__item">
             <div className="contents__item-logo">
               <Logo />
